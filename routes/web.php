6b953ded375e7253fc54cc3db1c245a213baa876
nttu94507 <nttu94507@gmail.com>
2021-10-25 22:32:00 +0800

    Fixed controllor error

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Uploadfile;
+use App\Http\Controllers\UploadFile;
 use App\Http\Controllers\Users;
 /*
 |--------------------------------------------------------------------------
