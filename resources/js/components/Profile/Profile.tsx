2bf56e73e74c059c4630244220fab18af43640ec
nttu94507 <nttu94507@gmail.com>
2021-12-27 18:54:32 +0800

    add profile

---
+++ resources/js/components/Profile/Profile.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Archery from "../Archery";
+import Master from "../Master";
+
+const Profile =()=>{
+    return(
+        <div>
+        {/* <Archery/> */}
+        <Master/>
+        </div>
+    )
+}
+
+export default Profile;
